a9e1598f9402c81588d5903d790df4ef64541a8b
amaziero <alison_lens@hotmail.com>
2020-12-14 21:21:19 -0300

    test: ensure SignUp controller returns status 400 if no password confirmation is provided

--- clean-node-api/src/presentation/controllers/signup.spec.ts
+++ clean-node-api/src/presentation/controllers/signup.spec.ts
@@ -58,4 +58,23 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
+
+  test('should return 400 if no password is provided', () => {
+    // sut stands for 'Sistem Under Test', tha means for the class wich is
+    // been tested at the moment
+
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
