176133f327bdc91b51660a7ee8397d99ec34ce2e
amaziero <alison_lens@hotmail.com>
2020-12-15 19:45:37 -0300

    feat: ensure signup controller returns 400 if an invalid email is provided

--- clean-node-api/src/presentation/controllers/SignUp.ts
+++ clean-node-api/src/presentation/controllers/SignUp.ts
@@ -1,9 +1,16 @@
+import { InvalidParamError } from '../errors/invalid-params-error'
 import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helpers/http-helper'
+import { IEmailValidador } from './protocols/IEmailValidator'
 import { IhttpRequest, IhttpResponse } from './protocols/Ihttp'
 import { ISignUpController } from './protocols/ISignUpController'
 
 export class SignUpController implements ISignUpController {
+  private readonly emailValidator: IEmailValidador
+  constructor(emailValidator: IEmailValidador) {
+    this.emailValidator = emailValidator
+  }
+
   handle(httpRequest: IhttpRequest): IhttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -11,6 +18,12 @@ export class SignUpController implements ISignUpController {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     return {
---
+++ clean-node-api/src/presentation/controllers/protocols/IEmailValidator.ts
@@ -0,0 +1,3 @@
+export interface IEmailValidador {
+  isValid: (email: string) => boolean
+}
