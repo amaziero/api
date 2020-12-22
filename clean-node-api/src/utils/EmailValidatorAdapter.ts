3fb1cd8dce76b3cc7e74e954799f4984a3dab4fb
amaziero <alison_lens@hotmail.com>
2020-12-22 19:53:28 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- clean-node-api/src/utils/EmailValidatorAdapter.ts
+++ clean-node-api/src/utils/EmailValidatorAdapter.ts
@@ -1,7 +1,8 @@
 import { IEmailValidador } from '../presentation/controllers/protocols/IEmailValidator'
+import validator from 'validator'
 
 export class EmailValidatorAdapter implements IEmailValidador {
   isValid(email: string): boolean {
-    return false
+    return validator.isEmail(email)
   };
 }
