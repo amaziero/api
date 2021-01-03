af4d60777cdb6ba1cfbae30b081bed9777fb9696
amaziero <alison_lens@hotmail.com>
2021-01-03 19:55:00 -0300

    feat: ensure BcryptAdapcter returns a hash on sucess

--- clean-node-api/src/infra/criptography/BcryptAdapcter.spec.ts
+++ clean-node-api/src/infra/criptography/BcryptAdapcter.spec.ts
@@ -8,6 +8,12 @@ const makeSut = (): any => {
   return { sut, salt }
 }
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return await new Promise(resolve => resolve('hash'))
+  }
+}))
+
 describe('BcryptAdapter', () => {
   test('should call bcrypt with correct value', async () => {
     const { sut, salt } = makeSut()
@@ -15,5 +21,12 @@ describe('BcryptAdapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('should return hashed value on sucess', async () => {
+    const { sut } = makeSut()
+    const hash = await sut.encrypt('hash')
+
+    expect(hash).toBe('hash')
   })
 })
