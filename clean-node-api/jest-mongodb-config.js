6da19a26c94794d39354bec0065a684d71c5559e
amaziero <alison_lens@hotmail.com>
2021-02-01 19:43:26 -0300

    chore: add jest configs for mongodb

---
+++ clean-node-api/jest-mongodb-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mongodbMomoryServerOptions: {
+    intance: {
+      dbName: 'jest'
+    },
+    binary: {
+      version: '4.0.3',
+      skipMD5: true
+    },
+    autoStrat: false
+  }
+}
