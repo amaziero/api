fef0d7c04bf84cfebe22a99186298ef63f5eba31
amaziero <alison_lens@hotmail.com>
2021-02-01 22:37:32 -0300

    delete: deleted maper file

--- clean-node-api/src/infra/db/mongodb/account-repository/accountMaper.ts
+++
@@ -1,7 +0,0 @@
-import { IAccountModel } from '../../../../domain/models/IAccountModel'
-
-export const map = (account: any): IAccountModel => {
-  const { _id, ...accountWithoutId } = account
-
-  return Object.assign({}, accountWithoutId, { id: _id })
-}
