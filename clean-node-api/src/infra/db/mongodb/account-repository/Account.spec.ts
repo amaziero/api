f2e21e855230fdd5622c34e13741be6fba48c57b
amaziero <alison_lens@hotmail.com>
2021-02-01 21:14:15 -0300

    test: ensure accountmongorepository returns an account on success

---
+++ clean-node-api/src/infra/db/mongodb/account-repository/Account.spec.ts
@@ -0,0 +1,33 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    const MONGO_URL_CHECKED = process.env.MONGO_URL
+    if (!MONGO_URL_CHECKED) {
+      throw Error('Mongo URL Error')
+    }
+
+    await MongoHelper.connect(MONGO_URL_CHECKED)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should Return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const account = await sut.add({
+      name: 'john_doe',
+      email: 'john_doe@any.com',
+      password: 'john_doe'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('john_doe')
+    expect(account.email).toBe('john_doe@any.com')
+    expect(account.password).toBe('john_doe')
+  })
+})
